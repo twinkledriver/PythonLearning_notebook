55f1199acbba3950af75a043adb9214b0b9a3eff
Gao Shengxu <1059067158@qq.com>
2016-04-05 20:27:07 +0800

    Learning....

--- python_notebook/python_notebook.py
+++ python_notebook/python_notebook.py
@@ -428,7 +428,11 @@ data=pd.merge(pd.merge(ratings,users),movies)
 
 data.ix[0]
 
-mean_ratings=data.pivot_table('rating',rows='title',cols='gender',aggfunc='mean')
+mean_ratings=data.pivot_table('rating',columns=['gender'],aggfunc='mean')
+#有错不支持rows了 没有定义 而 col也要写成columns rows改成index
+mean_ratings = data.pivot_table('rating',index='title',columns='gender',aggfunc = 'mean')
+
+mean_ratings[:5]
 
 
 #补充练习：http://python.jobbole.com/81212/   关于pivot_table的使用
@@ -481,103 +485,122 @@ pd.pivot_table(df,index=["Manager","Status"],values=["Price"],
 
 pd.pivot_table(df,index=["Manager","Status"],columns=["Product"],values=["Quantity","Price"],
                aggfunc={"Quantity":len,"Price":np.sum},fill_value=0)
+#***************************
+P33
 
-#******************************************
-#爬虫抓取，原地址：http://python.jobbole.com/84349/
+ratings_by_title=data.groupby('title').size();
 
-#-*-coding:utf8-*-
-#
-# import re
-# import string
-# import sys
-# import os
-# import urllib
-# import urllib2
-# from bs4 import BeautifulSoup
-# import requests
-# from lxml import etree
-#
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
-# if(len(sys.argv)&gt;=2):
-#     user_id = (int)(sys.argv[1])
-# else:
-#     user_id = (int)(raw_input(u"请输入user_id: "))
-#
-# cookie = {"Cookie": "#your cookie"}
-# url = 'http://weibo.cn/u/%d?filter=1&amp;page=1'%user_id
-#
-# html = requests.get(url, cookies = cookie).content
-# selector = etree.HTML(html)
-# pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
-#
-# result = ""
-# urllist_set = set()
-# word_count = 1
-# image_count = 1
-#
-# print u'爬虫准备就绪...'
-#
-# for page in range(1,pageNum+1):
-#
-#   #获取lxml页面
-#   url = 'http://weibo.cn/u/%d?filter=1&amp;page=%d'%(user_id,page)
-#   lxml = requests.get(url, cookies = cookie).content
-#
-#   #文字爬取
-#   selector = etree.HTML(lxml)
-#   content = selector.xpath('//span[@class="ctt"]')
-#   for each in content:
-#     text = each.xpath('string(.)')
-#     if word_count &gt;= 4:
-#       text = "%d :"%(word_count-3) +text+"\n\n"
-#     else :
-#       text = text+"\n\n"
-#     result = result + text
-#     word_count += 1
-#
-#   #图片爬取
-#   soup = BeautifulSoup(lxml, "lxml")
-#   urllist = soup.find_all('a',href=re.compile(r'^http://weibo.cn/mblog/oripic',re.I))
-#   first = 0
-#   for imgurl in urllist:
-#     urllist_set.add(requests.get(imgurl['href'], cookies = cookie).url)
-#     image_count +=1
-#
-# fo = open("/Users/Personals/%s"%user_id, "wb")
-# fo.write(result)
-# word_path=os.getcwd()+'/%d'%user_id
-# print u'文字微博爬取完毕'
-#
-# link = ""
-# fo2 = open("/Users/Personals/%s_imageurls"%user_id, "wb")
-# for eachlink in urllist_set:
-#   link = link + eachlink +"\n"
-# fo2.write(link)
-# print u'图片链接爬取完毕'
-#
-# if not urllist_set:
-#   print u'该页面中不存在图片'
-# else:
-#   #下载图片,保存在当前目录的pythonimg文件夹下
-#   image_path=os.getcwd()+'/weibo_image'
-#   if os.path.exists(image_path) is False:
-#     os.mkdir(image_path)
-#   x=1
-#   for imgurl in urllist_set:
-#     temp= image_path + '/%s.jpg' % x
-#     print u'正在下载第%s张图片' % x
-#     try:
-#       urllib.urlretrieve(urllib2.urlopen(imgurl).geturl(),temp)
-#     except:
-#       print u"该图片下载失败:%s"%imgurl
-#     x+=1
-#
-# print u'原创微博爬取完毕，共%d条，保存路径%s'%(word_count-4,word_path)
-# print u'微博图片爬取完毕，共%d张，保存路径%s'%(image_count-1,image_path)
-#
-# python web_spider.py 2549228714
-#**********************
+ratings_by_title[:10]
+
+active_titles=ratings_by_title.index[ratings_by_title>=250]
+active_titles
+
+mean_ratings=mean_ratings.ix[active_titles]
+
+mean_ratings
+
+top_female_ratings=mean_ratings.sort_index(by='F',ascending=False)
+
+top_female_ratings[:10]
+
+#*************************************
+P34
+mean_ratings['diff']=mean_ratings['M']-mean_ratings['F']
+sorted_by_diff=mean_ratings.sort_index(by='diff')
+sorted_by_diff[:15]
+sorted_by_diff[::-1][:15]
+#*************************************
+P35
+
+#标准差
+
+rating_std_by_title=data.groupby('title')['rating'].std()
+
+rating_std_by_title=rating_std_by_title.ix[active_titles]
+
+rating_std_by_title.order(ascending=False)[:10]
+
+#电影评分分析 结束
+#*********************
+P37
+#婴儿姓名分析
+
+
+#暂时只能放桌面读取，有文件夹就报错,不知为何？
+#文件名每个子目录必须大写首字母
+import pandas as pd
+names1880=pd.read_csv('C:\Users\Administrator\Desktop\yob1880.txt',names=['names','sex','births'])
+
+names1880.groupby('sex').births.sum();
+
+#定义年份，为了标记
+years=range(1980,2011);
+pieces=[];
+columns=['name','sex','births']
+
+#注意缩进 代表循环 内外部
+for year in years:
+    path='C:\Users\Administrator\Desktop\Names\yob%d.txt'%year
+    frame=pd.read_csv(path,names=columns)
+    frame['year']=year
+    pieces.append(frame)
+
+names=pd.concat(pieces,ignore_index=True)
+
+names
+
+total_births=names.pivot_table('births',index=['year'],columns=['sex'],aggfunc='sum')
+#只列出尾几行数据
+total_births.tail()
+
+#画图
+total_births.set_ylim(0,2500000)
+total_births.plot(title='Total births by sex and year')
+
+
+#比例化
+def add_prop(group):
+    births=group.births.astype(float)
+
+    group['prop']=births/births.sum()
+    return group
+
+names=names.groupby(['year','sex']).apply(add_prop)
+
+names
+
+#验证是否归一
+
+np.allclose(names.groupby(['year','sex']).prop.sum(),1)
+
+
+#前1000个
+def get_top1000(group):
+    return group.sort_index(by='births',ascending=False)[:1000]
+
+grouped=names.groupby(['year','sex'])
+top1000=grouped.apply(get_top1000)
+
+#另一种
+pieces_another=[]
+for year,group in names.groupby(['year','sex']):
+    pieces_another.append(group.sort_index(by='births',ascending=False)[:1000])
+
+top1000_another=pd.concat(pieces_another,ignore_index=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
